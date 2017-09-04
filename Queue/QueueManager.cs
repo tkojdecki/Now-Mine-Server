2fb1d6b85b2653d5fe1fc0e5e95cad20f342c821
krzanows <krzanows@gmail.com>
2017-09-04 22:25:57 +0200

    Cleanup #2

--- Queue/QueueManager.cs
+++ Queue/QueueManager.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using NowMine.Helpers;
 using System.ComponentModel;
